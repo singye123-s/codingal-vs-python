c892753db2be3efe1502d7c1cd49ba11ac07cff9
singye123-s <chaaand123@gmail.com>
2025-11-30 19:55:12 -0800

    student regisration

---
+++ variables.py
@@ -0,0 +1,10 @@
+#storing dat of a student in variables
+name = input("enter your name: ")
+address = input("enter your address: ")
+age = float(input("enter your age: "))
+phone_number = int(input("enter your phone number: "))
+
+print("name of student: ", name)
+print("address of student: ", address)
+print("age of student: ", age)
+print("phone number of student: ", phone_number)
