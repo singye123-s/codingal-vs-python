3ba3ae66296d20a29efac128bfda0b994c71638e
singye123-s <chaaand123@gmail.com>
2025-12-14 19:45:52 -0800

    average tree height

---
+++ average.py
@@ -0,0 +1,7 @@
+tree1 = int(input("enter height of tree: "))
+tree2 = int(input("enter height of tree: "))
+tree3 = int(input("enter height of tree: "))
+tree4 = int(input("enter height of tree: "))
+tree5 = int(input("enter height of tree: "))
+totaltree= tree1+tree2+tree3+tree4+tree5
+print(totaltree//5)
