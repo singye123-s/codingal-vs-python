c77d693a58023663ced0f93cedefb76c7cee66e9
singye123-s <chaaand123@gmail.com>
2025-12-28 08:07:50 -0800

    squareroot

---
+++ squareroot.py
@@ -0,0 +1,4 @@
+
+num = float(input("Enter a number: "))
+square_root = num ** 0.5
+print("The square root is:", square_root)
---
+++ swap.py
@@ -0,0 +1,22 @@
+a = 2
+b = 3
+temp = a
+a = b
+b = temp
+print(b)
+print(a)
+
+days = int(input("how many days: "))
+
+months = days//30
+remaining = days % 30
+
+print(months,"months and", remaining, "remaining days")
+
+number1 = float(input(" enter a number to raise to the power: "))
+number2 = float(input("enter another number to raise to the power: "))
+print(number1**number2)
+
+
+
+
