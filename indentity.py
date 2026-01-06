cb839f16532813617e5ca23de67f63524b77bb98
singye123-s <chaaand123@gmail.com>
2026-01-05 20:47:27 -0800

    identity

---
+++ indentity.py
@@ -0,0 +1,10 @@
+x = 5
+if type (x) is int:
+    print("true")
+else:
+    print('false')
+x = 5.0
+if type (x) is float:
+    print("true")
+else:
+    print("false")
--- notequalto.py
+++ notequalto.py
@@ -2,5 +2,5 @@ num = int(input("enter a number: "))
 if num%2 != 0:
     print("your number is odd!")
 else:
-    print("its even!")
+    print("its even!;")
 
