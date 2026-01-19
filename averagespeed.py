fec67c998ba0dc7fe7666bfa49e18f67076b835b
singye123-s <chaaand123@gmail.com>
2026-01-18 20:00:21 -0800

    cycle

---
+++ averagespeed.py
@@ -0,0 +1,11 @@
+cycle1 = 10
+cycle2 = 20
+cycle3 = 30
+average = (10+20+30)/3
+if cycle1 >= average:
+    print("cycle1 is good!")
+if cycle2 >= average:
+    print("cycle 2 is good!")
+if cycle3 >= average:
+    print("cycle 3 is good!")
+    
