5665f333d66a2c2364938717b1abdb96448e748e
singye123-s <chaaand123@gmail.com>
2026-02-08 20:02:53 -0800

    ghg

--- Module 2/powercalculator.py
+++ Module 2/powercalculator.py
@@ -1,9 +1,8 @@
 
 base = float(input("Enter the base number: "))
 power = int(input("Enter the power (n): "))
+res = 1
+for i in range(power):
+    res = res *base
+print(res)
 
-
-result = base ** power
-
-
-print("Result =", result)
