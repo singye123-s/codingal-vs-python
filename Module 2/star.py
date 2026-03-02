92f6b69167c40019ce429139bdee997b547af667
singye123-s <chaaand123@gmail.com>
2026-03-01 20:05:18 -0800

    efef

---
+++ Module 2/star.py
@@ -0,0 +1,11 @@
+import turtle
+import time
+turtle.Screen().bgcolor("white")
+turtle.Screen().setup(400,600)
+turtle = turtle.Turtle()
+for i in range(0,3):
+ turtle.forward(30)
+ turtle.right(120)
+ 
+turtle.done()
+
